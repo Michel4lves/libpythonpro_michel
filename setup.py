bcdf910364a2a7d9aecae46d578602cb6950e859
Michel4lves <michelsantosa@gmail.com>
2022-03-01 14:39:36 -0300

    Ajustes gerais

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro-michel-michel"
+PACKAGE = "libpythonpro-michel"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Michel Alves"
